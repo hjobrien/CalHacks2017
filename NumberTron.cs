226b45442c9d2931b683aad729ca07d9e2d2a9c0
Liam Kreiss <liamkreiss@airbears2-10-142-137-83.airbears2.1918.berkeley.edu>
2017-10-07 01:30:25 -0700

    added fields

--- NumberTron.cs
+++ NumberTron.cs
@@ -10,6 +10,8 @@ namespace NumberTron {
     string name;
     string number;
     int tailSize;
+    int[] position; //x, y positions
+    string direction;
 
     Player (string name) {
         this.name = name;
@@ -20,6 +22,42 @@ namespace NumberTron {
 
     void increaseTail() {
       this.tailSize += 1;
+    }
+
+    void spawnPlayer(int x, int y, string direction) {
+      this.position = new int[] {x, y};
+      this.direction = direction;
+    }
+
+    void turnLeft() {
+      this.direction = "left";
+    }
+
+    void turnRight() {
+      this.direction = "right";
+    }
+
+    void turnForward() {
+      this.direction = "forward";
+    }
+
+    void turnBackward() {
+      this.direction = "backword";
+    }
+
+    void moveAhead() {
+      if (this.direction.equals("left")) {
+        position[0] = position[0] - 1;
+      } else if (this.direction.equals("right")) {
+        position[0] = position[0] + 1;
+      } else if (this.direction.equals("forward")) {
+        position[1] = position[1] + 1;
+      } else if (this.direction.equals("backward")) {
+        position[1] = position[1] - 1;
+      } else {
+        Console.WriteLine("moveAhead Error") ;
+        Console.ReadLine() ;
+      }
     }
   }
 
